c7357ed0c441ee67d171fd9b7ac46f0fe7689254
ganzi01234 <229637012@qq.com>
2019-08-19 12:30:51 +0800

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,52 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <audio_recorder/AudioRecorderPlugin.h>
+#import <city_pickers/CityPickersPlugin.h>
+#import <device_info/DeviceInfoPlugin.h>
+#import <firebase_admob/FirebaseAdMobPlugin.h>
+#import <flutter_alipay/FlutterAlipayPlugin.h>
+#import <flutter_downloader/FlutterDownloaderPlugin.h>
+#import <flutter_mobile_vision_jamel/FlutterMobileVisionPlugin.h>
+#import <flutter_sound/FlutterSoundPlugin.h>
+#import <flutter_toolslib/RecorderWavPlugin.h>
+#import <flutter_webview_plugin/FlutterWebviewPlugin.h>
+#import <flutter_wechat/FlutterWechatPlugin.h>
+#import <fm_baidu_map/FmBaiduMapPlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
+#import <open_file/OpenFilePlugin.h>
+#import <package_info/PackageInfoPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <permission/PermissionPlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AudioRecorderPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioRecorderPlugin"]];
+  [CityPickersPlugin registerWithRegistrar:[registry registrarForPlugin:@"CityPickersPlugin"]];
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
+  [FLTFirebaseAdMobPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAdMobPlugin"]];
+  [FlutterAlipayPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAlipayPlugin"]];
+  [FlutterDownloaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterDownloaderPlugin"]];
+  [FlutterMobileVisionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterMobileVisionPlugin"]];
+  [FlutterSoundPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterSoundPlugin"]];
+  [RecorderWavPlugin registerWithRegistrar:[registry registrarForPlugin:@"RecorderWavPlugin"]];
+  [FlutterWebviewPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebviewPlugin"]];
+  [FlutterWechatPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWechatPlugin"]];
+  [FmBaiduMapPlugin registerWithRegistrar:[registry registrarForPlugin:@"FmBaiduMapPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [OpenFilePlugin registerWithRegistrar:[registry registrarForPlugin:@"OpenFilePlugin"]];
+  [FLTPackageInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPackageInfoPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PermissionPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
+}
+
+@end
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+//import 'package:flutter_web_ui/ui.dart' as ui;
+//// 将flutter_web_challenge_googlemaps替换为自己的package
+//
+//main() async {
+//  await ui.webOnlyInitializePlatform();
+//  app.main();
+//}
